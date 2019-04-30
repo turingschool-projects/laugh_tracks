e3d8fb1f72d291dd472b521303da89b082269203
William Homer <billyhomeriii@gmail.com>
2019-04-30 14:52:02 -0600

    remove old comdedians table format

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20190430031638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comdedians", force: :cascade do |t|
-    t.string "name"
-    t.string "age"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comedians", force: :cascade do |t|
     t.string "name"
     t.string "age"
