86f32d7e15cb583d1d9e02461006dddcb879c4f6
James Cape <james.cape@gmail.com>
2019-04-30 21:40:57 -0600

    Removes save_and_open_page

--- spec/features/user_sees_all_comedians_spec.rb
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -7,7 +7,7 @@ describe "user sees all comedians" do
         comedian_2 = Comedian.create(name: "Joe Rogan", age: 51, birthplace: "Newark, NJ", image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwil2d6_-YvhAhUR0IMKHSTACf4QjRx6BAgBEAU&url=https%3A%2F%2Fwaaf.radio.com%2Fblogs%2Fanthony-capobianco%2F13-rockers-have-appeared-joe-rogan-experience&psig=AOvVaw1DfyJPblSkSVLayC7G5let&ust=1553007377857892")
 
         visit '/comedians'
-
+        # save_and_open_page
         expect(page).to have_content(comedian_1.name)
         expect(page).to have_content(comedian_2.name)
     end
