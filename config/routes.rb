c1bbe92924d967130ee4b468bb02584c4d5a1c09
James Cape <james.cape@gmail.com>
2019-04-30 18:55:06 -0600

    Creates comedian model test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/', to: 'comedians#index'
+  get '/comedians', to: 'comedians#index'
 
   # get '/comedians', to: 'comedians#index'
   # User Story 1
