693d7cc28e55a44b307aedfdfe43cf63936a4d70
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-01 11:19:41 -0600

    Adds age search route, look to refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :comedians, only: [:index]
+
+  #this the best way of making this route?
+  get "/comedians?age={:age=> :age}", to: 'comedians#index'
 end
