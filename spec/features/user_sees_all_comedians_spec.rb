aecb525a8bcee59fb25c2c00110577120e8128b5
James Cape <james.cape@gmail.com>
2019-04-30 19:14:12 -0600

    Writes initial comedian feature test

---
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "user sees all comedians" do
+  describe "they visit /comedians" do
+    it "displays all comedians" do
+        comedian_1 = Comedian.create(name: "Bill Burr", age: 50, birthplace: "Canton, MA", image_url: "https://image.masslive.com/home/mass-media/width960/img/entertainment/photo/nashville-comedy-festival-21d347107e975672.jpg")
+        comedian_2 = Comedian.create(name: "Joe Rogan", age: 51, birthplace: "Newark, NJ", image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwil2d6_-YvhAhUR0IMKHSTACf4QjRx6BAgBEAU&url=https%3A%2F%2Fwaaf.radio.com%2Fblogs%2Fanthony-capobianco%2F13-rockers-have-appeared-joe-rogan-experience&psig=AOvVaw1DfyJPblSkSVLayC7G5let&ust=1553007377857892")
+
+        visit '/comedians'
+
+        expect(page).to have_content(comedian_1.name)
+        expect(page).to have_content(comedian_2.name)
+    end
+  end
+end
