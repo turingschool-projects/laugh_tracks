c168416da7e30aa463e0d39f1bb2bf113f5845a4
David Hardy <davehardy632@gmail.com>
2019-04-30 23:24:45 -0600

    Link implemented to form, route added aswell

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/comedians/:age', to: 'comedians#age', as: 'comedian_age'
 
-  resources :comedians, only: [:index]
+  resources :comedians, only: [:index, :new]
 end
