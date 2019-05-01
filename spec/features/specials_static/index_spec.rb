27620c4451c33aeb07454da3b29d9aace53718b0
davisc0801 <davisc1014@gmail.com>
2019-05-01 15:26:26 -0600

    Add feature test for comedian specials

--- spec/features/specials_static/index_spec.rb
+++ spec/features/specials_static/index_spec.rb
@@ -1,8 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "specials index page" do
+RSpec.describe "specials index page", type: :feature do
 
   before (:each) do
+    @special_1 = Special.create(name: "Comedy Central Special", time: 37)
+    @special_2 = Special.create(name: "Mitch All Together", time: 43)
+    @special_3 = Special.create(name: "Kid Gorgeous at Radio City", time: 65)
+    @special_4 = Special.create(name: "The Comeback Kid", time: 62)
+    @special_5 = Special.create(name: "New in Town", time: 60)
   end
 
   it "loads a page" do
@@ -11,9 +16,24 @@ RSpec.describe "specials index page" do
     expect(page.status_code).to eq(200)
     expect(current_path).to eq("/comedians")
   end
-
+  #unsure of final layout, will target once design is complete.
+  # within("div#comedian_list") do
   it "shows specials information" do
-    
+    visit "/comedians"
+
+    #unsure of final layout, will target once design is complete.
+    # within("div#comedian_list") do
+      expect(page).to have_content(@special_1.name)
+      expect(page).to have_content(@special_1.time)
+      expect(page).to have_content(@special_2.name)
+      expect(page).to have_content(@special_2.time)
+      expect(page).to have_content(@special_3.name)
+      expect(page).to have_content(@special_3.time)
+      expect(page).to have_content(@special_4.name)
+      expect(page).to have_content(@special_4.time)
+      expect(page).to have_content(@special_5.name)
+      expect(page).to have_content(@special_5.time)
+    # end
   end
 
 end
