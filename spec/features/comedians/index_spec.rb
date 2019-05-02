d74ee5e3b9c965f0c144c9efe28507b335036be1
Andrew Johnson <manilda7@gmail.com>
2019-05-02 11:43:32 -0600

    Adds user story 1 functionality

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -1,4 +1,15 @@
 require "rails_helper"
 
 RSpec.describe "songs index page", type: :feature do
-  if ""
+  it "user can see all comedians" do
+    comedian_1 = Comedian.create(name: "Bill Burr", age: 50, birthplace: "Canton, MA")
+    comedian_2 = Comedian.create(name: "Joe Rogan", age: 51, birthplace: "Newark, NJ")
+
+    visit "/comedians"
+
+    expect(page).to have_content(comedian_1.name)
+    #expect(page).to have_content("Bill Burr")
+    expect(page).to have_content(comedian_2.name)
+    #expect(page).to have_content("Joe Rogan")
+  end
+end
