1eeac6516c46d5efea914a6daf88d9e65fc42da5
Justin Duncan <falsemotive@icloud.com>
2019-05-02 17:43:02 -0600

    Add test for checking unique hometowns on page

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 RSpec.describe "a vistor visiting the players index path" do
-  attr_reader :player_1, :player_2
+  attr_reader :players, :player_1, :player_2, :player_3
 
   before :each do
     Player.destroy_all
    
     @player_1 = Player.create(name: "Joe", age: 31, hometown: "Springfield, MA", image: "https://cdn.dribbble.com/users/567082/screenshots/4356358/profile_picture.png")
-    @player_2 = Player.create(name: "Jesse", age:41, hometown: "Galaxy, One", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcsgCEvJH9nG7sQdxuiB5ZFcYdO77YO3f10Ig-lOdCYxX9HrKb")
-
+    @player_2 = Player.create(name: "Jesse", age:41, hometown: "Springfield, MA", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcsgCEvJH9nG7sQdxuiB5ZFcYdO77YO3f10Ig-lOdCYxX9HrKb")
+    @player_3 = Player.create(name: "Bob", age:56, hometown: "Boston", image:"http:image.png")
     @player_1.cards.create(name: "Blue Dragon", cost:70, description: "This is the dragon to end all dragons")
+    
+    @players = [player_1,player_2,player_3]
   end
   
   it "should see a list of players" do 
@@ -23,9 +25,9 @@ RSpec.describe "a vistor visiting the players index path" do
     end
 
     within "#player-#{player_2.id}" do
-      expect(page).to have_content("Jesse")
-      expect(page).to have_content("41")
-      expect(page).to have_content("Hometown: Galaxy, One")
+      expect(page).to have_content(player_2.name)
+      expect(page).to have_content(player_2.age)
+      expect(page).to have_content("Hometown: #{player_2.hometown}")
       expect(find("#player-#{player_2.id}-image")[:src]).to have_content(player_2.image)
     end
 
@@ -62,6 +64,17 @@ RSpec.describe "a vistor visiting the players index path" do
       expect(page).to_not have_content(player_2.name)
     end
 
+  end
+
+  describe "Statistics are shown at top of page" do 
+   
+    it "should see an area marked Statistics on page" do
+      visit "/players"
+
+      expect(page).to have_content("Statistics")
+      expect(page).to have_content("Avg Age: #{Player.avg_age.to_i}")
+      expect(page).to have_content("Hometowns: #{player_1.hometown}")
+    end
   end
 
 end
