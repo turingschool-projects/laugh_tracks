246040dd175144cc90e5452ad1e5c8f2f2ab05b9
James Cape <james.cape@gmail.com>
2019-05-02 13:43:29 -0600

    Changes xit's to it's

--- spec/features/comedian_landing_spec.rb
+++ spec/features/comedian_landing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "user sees all comedians" do
 
   describe "they visit /comedians" do
-    xit "displays all comedians" do
+    it "displays all comedians" do
       ActiveRecord::Base.connection.reset_pk_sequence!('comedians')
       comedian_1 = Comedian.create(name: "Bill Burr", age: 50, birthplace: "Canton, MA", image_url: "https://image.masslive.com/home/mass-media/width960/img/entertainment/photo/nashville-comedy-festival-21d347107e975672.jpg")
       comedian_2 = Comedian.create(name: "Joe Rogan", age: 51, birthplace: "Newark, NJ", image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwil2d6_-YvhAhUR0IMKHSTACf4QjRx6BAgBEAU&url=https%3A%2F%2Fwaaf.radio.com%2Fblogs%2Fanthony-capobianco%2F13-rockers-have-appeared-joe-rogan-experience&psig=AOvVaw1DfyJPblSkSVLayC7G5let&ust=1553007377857892")
@@ -14,7 +14,7 @@ describe "user sees all comedians" do
         expect(page).to have_content(comedian_2.name)
     end
 
-    xit "displays comedian info in the specific comedian area" do
+    it "displays comedian info in the specific comedian area" do
 
       ActiveRecord::Base.connection.reset_pk_sequence!('comedians')
 
@@ -52,7 +52,7 @@ describe "user sees all comedians" do
 
 
       visit '/comedians'
-
+      # save_and_open_page
       expect(page).to have_content("Walk Your Way Out")
       expect(page).to have_content("I'm Sorry You Feel That Way")
       expect(page).to have_content("You People Are All The Same")
