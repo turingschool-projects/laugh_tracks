700b03fad40febba09c79a6596e39af8f23d2033
davisc0801 <davisc1014@gmail.com>
2019-05-01 18:58:54 -0600

    Add additional specificity to tests

--- spec/features/comedians_static/index_spec.rb
+++ spec/features/comedians_static/index_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 RSpec.describe "comedian index page", type: :feature do
 
   before(:each) do
-    @comedian_1 = Comedian.create(name: "John Mulany", age: 37, city: "Chicago")
-    @comedian_2 = Comedian.create(name: "Mitchell Hedberg", age: 37, city: "Saint Paul")
+    @comedian_1 = Comedian.create(name: "John Mulany", age: 37, city: "Chicago", \
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCCUVWI5B-6-80aviR_4WtoJqCsu33NEKB18j_xZ7UT4WpWhQa")
+    @comedian_2 = Comedian.create(name: "Mitchell Hedberg", age: 37, city: "Saint Paul", \
+    image: "https://pixel.nymag.com/imgs/daily/vulture/2018/05/02/02-john-mulaney.w700.h700.jpg")
   end
 
   it "loads a page" do
@@ -17,15 +19,35 @@ RSpec.describe "comedian index page", type: :feature do
   it "shows comedian demographics" do
     visit "/comedians"
 
-    #unsure of final layout, will target once design is complete.
-    # within("div#comedian_list") do
+    within("#comedian_#{@comedian_1.id}_info") do
       expect(page).to have_content(@comedian_1.name)
       expect(page).to have_content(@comedian_1.age)
       expect(page).to have_content(@comedian_1.city)
+    end
+    
+    within("#comedian_#{@comedian_2.id}_info") do
       expect(page).to have_content(@comedian_2.name)
       expect(page).to have_content(@comedian_2.age)
       expect(page).to have_content(@comedian_2.city)
+<<<<<<< Updated upstream
     # end
 
+=======
+    end
+  end
+
+  it "shows comedian thumbnail" do
+    visit "/comedians"
+    within("#comedian_#{@comedian_1.id}_info") do
+      actual = find("#comedian_#{@comedian_1.id}_thumbnail")[:src]
+      #possible placeholder id
+      expect(actual).to eq(@comedian_1.image)
+    end
+    within("#comedian_#{@comedian_2.id}_info") do
+      actual = find("#comedian_#{@comedian_2.id}_thumbnail")[:src]
+      #possible placeholder id
+      expect(actual).to eq(@comedian_2.image)
+    end
+>>>>>>> Stashed changes
   end
 end
