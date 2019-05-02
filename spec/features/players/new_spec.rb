bd2d8ae370815dc58e6679d3e9d5948cb77a858f
Justin Duncan <falsemotive@icloud.com>
2019-05-02 14:05:37 -0600

    Add test for create a player page

--- spec/features/players/new_spec.rb
+++ spec/features/players/new_spec.rb
@@ -6,19 +6,18 @@ RSpec.describe "A user visiting the /players/new path" do
 before :each do 
   Player.destroy_all
 
-  @player_1 = Player.create!("Joe", 31, "Miami", image: "http:www.image1.png")
-  @player_2 = Player.create!("Jane", 30, "Tokyo", image: "http:www.image2.png")
 end
 
   it "should create a new player" do 
     visit '/players/new'
+    #save_and_open_page
 
-    fill_in("name", :with => "Joel")
-    fill_in("age", :with => "21")
-    fill_in("hometown", :with => "Trenton, NJ")
-    fill_in("image", :with => "http://www.image3.png")
-    click_button("submit")
-    
+    fill_in("player_name", :with => "Joel")
+    fill_in("player_age", :with => "21")
+    fill_in("player_hometown", :with => "Trenton, NJ")
+    fill_in("player_image", :with => "http://www.image3.png")
+    click_button("commit")
+   save_and_open_page
     player_3 = Player.last
     expect(page).to have_content(player_3.name)
   end
