09f5e9dba121552aa8ffe87eb10cb6a7b1e74c72
davisc0801 <davisc1014@gmail.com>
2019-05-04 15:43:24 -0600

    Add test for page displaying a specials count for each comedian

--- spec/features/specials_static/index_spec.rb
+++ spec/features/specials_static/index_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 RSpec.describe "As a visitor", type: :feature do
 
   before (:each) do
-    comedian_1 = Comedian.create(name: "Mitchell Hedberg", age: 37, city: "Saint Paul", \
+    @comedian_1 = Comedian.create(name: "Mitchell Hedberg", age: 37, city: "Saint Paul", \
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCCUVWI5B-6-80aviR_4WtoJqCsu33NEKB18j_xZ7UT4WpWhQa")
-    comedian_2 = Comedian.create(name: "John Mulany", age: 37, city: "Chicago", \
+    @comedian_2 = Comedian.create(name: "John Mulany", age: 37, city: "Chicago", \
     image: "https://pixel.nymag.com/imgs/daily/vulture/2018/05/02/02-john-mulaney.w700.h700.jpg")
-    comedian_3 = Comedian.create(name: "George Carlin", age: 71, city: "Manhattan", \
+    @comedian_3 = Comedian.create(name: "George Carlin", age: 71, city: "Manhattan", \
     image: "https://pixel.nymag.com/imgs/daily/vulture/2018/09/23/1-George-Carlin.w330.h330.jpg")
-    @special_1 = comedian_1.specials.create(name: "Comedy Central Special", time: 37)
-    @special_2 = comedian_1.specials.create(name: "Mitch All Together", time: 43)
-    @special_3 = comedian_2.specials.create(name: "Kid Gorgeous at Radio City", time: 65)
-    @special_4 = comedian_2.specials.create(name: "The Comeback Kid", time: 62)
-    @special_5 = comedian_2.specials.create(name: "New in Town", time: 60)
-    @special_6 = comedian_3.specials.create(name: "Jammin' in New York", time: 57)
+    @special_1 = @comedian_1.specials.create(name: "Comedy Central Special", time: 37)
+    @special_2 = @comedian_1.specials.create(name: "Mitch All Together", time: 43)
+    @special_3 = @comedian_2.specials.create(name: "Kid Gorgeous at Radio City", time: 65)
+    @special_4 = @comedian_2.specials.create(name: "The Comeback Kid", time: 62)
+    @special_5 = @comedian_2.specials.create(name: "New in Town", time: 60)
+    @special_6 = @comedian_3.specials.create(name: "Jammin' in New York", time: 57)
   end
 
   describe "when I visit /comedians" do
@@ -38,6 +38,14 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_content(@special_4.time)
       expect(page).to have_content(@special_5.name)
       expect(page).to have_content(@special_5.time)
+    end
+
+    it "shows a count of specials" do
+      visit "/comedians"
+
+      expect(page).to have_content(@comedian_1.specials.count)
+      expect(page).to have_content(@comedian_2.specials.count)
+      expect(page).to have_content(@comedian_3.specials.count)
     end
   end
 end
