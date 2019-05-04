cf3fd5d5b3d0eb5e889b73cf221e6df8c90b788f
Patrick Duvall <duvallp208@gmail.com>
2019-05-04 09:18:12 -0600

    Write city sort test, refactor name sort

---
+++ spec/features/user_may_sort_comedians_spec.rb
@@ -0,0 +1,33 @@
+# As a user, when I visit `/comedians?sort=name`
+# Then I see all previous information, but all comedians are
+# sorted alphabetically by the name of the comedians.
+
+require "rails_helper"
+
+describe "user may see sorted comedians" do
+  describe "they visit /comedians?sort=name" do
+    it "displays all comedians sorted by name " do
+      comedian_1 = Comedian.create(name: "george", age: 22, city: 'Montreal')
+      comedian_2 = Comedian.create(name: "bob", age: 34, city: 'Austin')
+      comedian_2 = Comedian.create(name: "xena", age: 39, city: 'Austin')
+      visit '/comedians?sort=name'
+      expect(/bob(.|\n)*george(.|\n)xena/).to match(page.body)
+    end
+  end
+
+  describe "they visit /comedians?sort=city" do
+    it "displays all comedians sorted by city " do
+
+      comedian_1 = Comedian.create(name: "george", age: 22, city: 'Montreal')
+      comedian_2 = Comedian.create(name: "bob", age: 34, city: 'Austin')
+      comedian_2 = Comedian.create(name: "xena", age: 39, city: 'Detroit')
+
+      visit '/comedians?sort=city'
+      save_and_open_page
+
+      expect(/Austin(.|\n)*Montreal/).to match(page.body)
+    end
+  end
+
+
+end
