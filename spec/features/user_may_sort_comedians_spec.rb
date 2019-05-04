39ad13647d4b1fe37a22fd8842161bb08432cfc8
Patrick Duvall <duvallp208@gmail.com>
2019-05-04 10:12:56 -0600

    Add age-sort test

--- spec/features/user_may_sort_comedians_spec.rb
+++ spec/features/user_may_sort_comedians_spec.rb
@@ -17,15 +17,20 @@ describe "user may see sorted comedians" do
 
   describe "they visit /comedians?sort=city" do
     it "displays all comedians sorted by city " do
-
       comedian_1 = Comedian.create(name: "george", age: 22, city: 'Detroit')
       comedian_2 = Comedian.create(name: "bob", age: 34, city: 'Austin')
       comedian_3 = Comedian.create(name: "xena", age: 39, city: 'Montreal')
-
       visit '/comedians?sort=city'
-save_and_open_page
-
       expect(/City: Austin(.|\n)*City: Detroit(.|\n)*City: Montreal/).to match(page.body)
+    end
+  end
+  describe "they visit /comedians?sort=age" do
+    it "displays all comedians sorted by age " do
+      comedian_1 = Comedian.create(name: "george", age: 104, city: 'Detroit')
+      comedian_2 = Comedian.create(name: "bob", age: 34, city: 'Austin')
+      comedian_3 = Comedian.create(name: "xena", age: 39, city: 'Montreal')
+      visit '/comedians?sort=age'
+      expect(/Age: 34(.|\n)*Age: 39(.|\n)*Age: 102/).to match(page.body)
     end
   end
 
