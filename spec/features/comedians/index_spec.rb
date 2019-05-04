fb2a65bf8c236b8c9d689394451c24c534a6850e
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-05-04 17:53:49 -0600

    Add tests for User Story 2 and 3

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe "comedians index page", type: :feature do
   # Then I see a list of comedians with name, age, and city
 
   it "user can see all comedians" do
-    andy = Comedian.create(name: "Andy Kauffman", age: 35, city: "New York, NY")
-    brian = Comedian.create(name: "Brian Regan", age: 60, city: "Miami, FL")
+    andy = Comedian.create(name: "Andy Kauffman", age: 35, city: "New York, NY", img_url: "andy.png")
+    brian = Comedian.create(name: "Brian Regan", age: 60, city: "Miami, FL", img_url: "brian.png")
 
     visit "/comedians"
 
@@ -18,5 +18,42 @@ RSpec.describe "comedians index page", type: :feature do
     expect(page).to have_content("(#{brian.age} years old) #{brian.city}")
   end
 
+  # As a visitor
+  # When I visit `/comedians`
+  # Then I also see a list of each comedian's TV specials, including
 
+  it "user can see comedian specials" do
+    # used multiple ways to create objects for future reference (.new is industry preferred)
+    andy = Comedian.new(name: "Andy Kauffman", age: 35, city: "New York, NY", img_url: "andy.png")
+    andy.save
+    brian = Comedian.create(name: "Brian Regan", age: 60, city: "Miami, FL", img_url: "brian.png")
+    special_1 = Special.new(name: "Andy Live in NY", run_time: 50)
+    special_1.comedian = andy
+    special_1.save
+    special_2 = Special.create(name: "Kauffman Live", run_time: 55, comedian_id: andy.id)
+    special_3 = Special.create(name: "Brian Live in LA", run_time: 70, comedian_id: brian.id)
+
+    visit "/comedians"
+
+    expect(page).to have_content(special_1.name)
+    expect(page).to have_content("(#{special_1.run_time} minutes)")
+    expect(page).to have_content(special_2.name)
+    expect(page).to have_content("(#{special_2.run_time} minutes)")
+    expect(page).to have_content(special_3.name)
+    expect(page).to have_content("(#{special_3.run_time} minutes)")
+
+  end
+
+  # As a visitor
+  # When I visit `/comedians`
+  # I see a thumbnail image for each comedian
+
+  it "user can see comedian images" do
+    andy = Comedian.create(name: "Andy Kauffman", age: 35, city: "New York, NY", img_url: "andy.png")
+    brian = Comedian.create(name: "Brian Regan", age: 60, city: "Miami, FL", img_url: "brian.png")
+
+    visit "/comedians"
+
+    expect(page).to have_css("img[src*='andy.png']")
+  end
 end
--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Comedian, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
     it { should validate_presence_of :city }
+    it { should validate_presence_of :img_url }
+  end
+
+  describe 'relationships' do
+    it { should have_many :specials }
   end
 
 end
