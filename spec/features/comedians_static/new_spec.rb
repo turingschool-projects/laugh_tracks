65cde4aef4ca37c2751eade60569edc1aaef4bde
davisc0801 <davisc1014@gmail.com>
2019-05-04 18:30:14 -0600

    Add tests for form functionality

--- spec/features/comedians_static/new_spec.rb
+++ spec/features/comedians_static/new_spec.rb
@@ -8,5 +8,28 @@ RSpec.describe "When I visit /comedians/new" do
       expect(page.status_code).to eq(200)
       expect(current_path).to eq("/comedians/new")
     end
+
+    it "has fields for name, age and hometown" do
+      visit "/comedians/new"
+
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Age")
+      expect(page).to have_content("Hometown")
+    end
+
+    it "can create a new comedian" do
+      visit "/comedians/new"
+
+      fill_in "Name", with: "Bo Burnham"
+      fill_in "Age", with: "28"
+      fill_in "City", with: "Hamilton"
+      click_on "Create Comedian"
+
+      expect(current_path).to eq("/comedians")
+      #target this if possible
+      expect(page).to have_content("Bo Burnham")
+      expect(page).to have_content("Age: 28")
+      expect(page).to have_content("Hometown: Hamilton")
+    end
   end
 end
