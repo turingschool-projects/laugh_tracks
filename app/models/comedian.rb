822175b8ca90cf557c640b02197bbe1591e62261
davisc0801 <davisc1014@gmail.com>
2019-05-05 23:04:34 -0600

    Add average age method to model

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -13,4 +13,32 @@ class Comedian < ApplicationRecord
   def specials_count
     self.specials.count
   end
+
+  def average_age(age)
+    if age != nil
+      Comedian.by_age(age).average(:age)
+      #can just replace this with age, but this is more accurate on what to do.
+    else
+      Comedian.average(:age)
+    end
+  end
+
+  def get_hometowns(age)
+    if age != nil
+      Comedian.by_age(age).select(:city).distinct
+      #can just replace this with age, but this is more accurate on what to do.
+    else
+      Comedian.select(:city).distinct
+    end
+  end
+
+  # def get_total_specials(age)
+  #   if age != nil
+  #     require 'pry'; binding.pry
+  #     Comedian.by_age(age).specials.count
+  #     #can just replace this with age, but this is more accurate on what to do.
+  #   else
+  #     Comedian.specials.count
+  #   end
+  # end
 end
